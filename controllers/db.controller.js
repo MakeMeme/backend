19629700b0b243acc5c182fc4b4f5519d726cee7
Jatin parmar <jatinparmar980@gmail.com>
2023-08-12 13:58:43 +0530

    Added Users & Memes Route

--- controllers/db.controller.js
+++ controllers/db.controller.js
@@ -18,4 +18,37 @@ const checkDb = async (req, res) => {
   }
 };
 
-module.exports = { checkDb };
+const getUsers = async (req, res) => {
+  logger.info(`${req.method}: ${req.originalUrl}`);
+  const dbInstance = await db.getInstance();
+
+  try {
+    const result = await dbInstance.query(queries.checkUsersTable());
+    logger.info(result.rows);
+    res.send({ data: result.rows });
+  } catch (error) {
+    logger.error(error);
+    res.status(400).send({ data: [] });
+  } finally {
+    dbInstance.release();
+  }
+};
+
+const getMemes = async (req, res) => {
+  logger.info(`${req.method}: ${req.originalUrl}`);
+  const dbInstance = await db.getInstance();
+
+  try {
+    const result = await dbInstance.query(queries.checkMemesTable());
+    logger.info(result.rows);
+    res.send({ data: result.rows });
+  } catch (error) {
+    logger.error(error);
+    res.status(400).send({ data: [] });
+  } finally {
+    dbInstance.release();
+  }
+};
+
+module.exports = { checkDb, getUsers, getMemes };
+
--- routes/db.route.js
+++ routes/db.route.js
@@ -1,7 +1,9 @@
 const express = require("express");
-const { checkDb } = require("../controllers/db.controller");
+const { checkDb, getUsers, getMemes } = require("../controllers/db.controller");
 const router = express.Router();
 
 router.get("/check", checkDb);
+router.get("/users", getUsers);
+router.get("/memes", getMemes);
 
 module.exports = router;
